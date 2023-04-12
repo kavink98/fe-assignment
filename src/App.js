91a4b2389db6903203e77bbfd028d5b75278ba52
Kavin <kavink98@gmail.com>
2023-04-12 02:51:09 -0400

    Changed the wallet static address

--- src/App.js
+++ src/App.js
@@ -11,62 +11,62 @@ import { Link, Route, Routes } from 'react-router-dom';
 import Login from './Components/Login';
 
 const TabPanel = (props) => {
-  const { children, value, index, ...other } = props;
+    const { children, value, index, ...other } = props;
 
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box sx={{ p: 3 }}>
-          {children}
-        </Box>
-      )}
-    </div>
-  );
+    return (
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`simple-tabpanel-${index}`}
+            aria-labelledby={`simple-tab-${index}`}
+            {...other}
+        >
+            {value === index && (
+                <Box sx={{ p: 3 }}>
+                    {children}
+                </Box>
+            )}
+        </div>
+    );
 }
 
 function App() {
 
-  const [value, setValue] = useState(0)
+    const [value, setValue] = useState(0)
 
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
-
-  function a11yProps(index) {
-    return {
-      id: `simple-tab-${index}`,
-      'aria-controls': `simple-tabpanel-${index}`,
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
     };
-  }
 
-  return (
+    function a11yProps(index) {
+        return {
+            id: `simple-tab-${index}`,
+            'aria-controls': `simple-tabpanel-${index}`,
+        };
+    }
 
-    <div className="App">
-      <AddressContext.Provider value="0xDBC05B1ECB4FDAEF943819C0B04E9EF6DF4BABD6">
-        <Box sx={{ flexGrow: 1 }}>
-          <AppBar position="static">
-            <Toolbar>
-              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                Blockchain Explorer
-              </Typography>
-              <Button component={Link} color="inherit" to="/">Home</Button>
-              <Button component={Link} color="inherit" to="/login">Login</Button>
-            </Toolbar>
-          </AppBar>
-        </Box>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
-      </AddressContext.Provider>
-    </div>
-  );
+    return (
+
+        <div className="App">
+            <AddressContext.Provider value="0x5CB19592A570BeB6171BdA547c5F5bb84EaEa8Ff">
+                <Box sx={{ flexGrow: 1 }}>
+                    <AppBar position="static">
+                        <Toolbar>
+                            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                                Blockchain Explorer
+                            </Typography>
+                            <Button component={Link} color="inherit" to="/">Home</Button>
+                            <Button component={Link} color="inherit" to="/login">Login</Button>
+                        </Toolbar>
+                    </AppBar>
+                </Box>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/login" element={<Login />} />
+                </Routes>
+            </AddressContext.Provider>
+        </div>
+    );
 }
 
 export default App;
